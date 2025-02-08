d2832f5ed2af03aae24fb5d27b7012230892c075
Desiree Perez <desiree.p.perez@gmail.com>
2025-02-07 23:07:13 -0700

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
-from .views import post_list
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
