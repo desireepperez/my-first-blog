d89a1a0b35c6cdd05740e0d36b64990458cfa89b
Desiree Perez <desiree.p.perez@gmail.com>
2025-02-07 23:40:45 -0700

    changes to ensure static file absolute path and to create base directory 'static' in mysite folder

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Added by me -- Ensure a path is an absolute path
+
+relative_path = Path('relative/directory/or/file')
+absolute_path = relative_path.resolve()
+print(absolute_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -120,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
